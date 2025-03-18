747e5711d887491ad550b34113e29bca99d838d2
mauriciotamez <vitxlssreturn@gmail.com>
2025-03-18 02:23:18 -0600

    feat: add basic health check test, refactor our project into a library and a binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+    let port = listener.local_addr()?.port();
+
+    println!("Server started on port {}", port);
+
+    run(listener)?.await?;
+
+    Ok(())
 }
