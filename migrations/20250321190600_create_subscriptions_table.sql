9e309d1a335d8c0b97e23875993465eab5f5acb5
mauriciotamez <vitxlssreturn@gmail.com>
2025-03-25 02:17:36 -0600

    chore(db): created pool to work with our db, spin up a brand-new logical database for each integration test

--- migrations/20250321190600_create_subscriptions_table.sql
+++ migrations/20250321190600_create_subscriptions_table.sql
@@ -1,8 +1,10 @@
 -- Add migration script here
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE subscriptions(
-    id uuid NOT NULL,
+    id uuid NOT NULL DEFAULT uuid_generate_v4(),
     PRIMARY KEY (id),
     email TEXT NOT NULL UNIQUE,
     name TEXT NOT NULL,
-    subscribed_at timestamptz NOT NULL
+    subscribed_at timestamptz NOT NULL DEFAULT NOW()
 );
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -6,6 +7,9 @@ use zero2prod::startup::run;
 async fn main() -> std::io::Result<()> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(&address)?;
 
@@ -13,5 +17,5 @@ async fn main() -> std::io::Result<()> {
     println!("Zero2Prod:: Listening on {} 🦀", address);
     println!(":::::::::::::::::::::::::::::::::::::::");
 
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
