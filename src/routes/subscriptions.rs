1281acea4527a9cf05ba4a0efc0341454b4ca1dd
mauriciotamez <vitxlssreturn@gmail.com>
2025-03-25 02:18:11 -0600

    feat: created a simple subscribe function to create a new record on our db

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,9 +1,36 @@
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
+
 #[derive(serde::Deserialize)]
+#[allow(dead_code)]
 pub struct FormData {
     email: String,
     name: String,
 }
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    sqlx::query!(
+        r#"
+INSERT INTO subscriptions (id, email, name, subscribed_at)
+VALUES ($1, $2, $3, $4)
+"#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    // We use `get_ref` to get an immutable reference to the `PgConnection`
+    // wrapped by `web::Data`.
+    .execute(pool.get_ref())
+    .await
+    .map_or_else(
+        |e| {
+            println!("Failed to execute query: {}.", e);
+            HttpResponse::InternalServerError().finish()
+        },
+        |_| HttpResponse::Ok().finish(),
+    );
     HttpResponse::Ok().finish()
 }
